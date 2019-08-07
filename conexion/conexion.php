4dcb957d327bd566f50708350a87be9e2c2d402b
adan <adanalfredogaleascarcamo@gmail.com>
2019-08-06 20:23:03 -0600

    html

---
+++ conexion/conexion.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jonatan Moncada
+ * Date: 24/7/2019
+ * Time: 19:33
+ */
+
+$conexion=mysqli_connect('localhost','root','dinorey64','programaciondenegocios') or
+                    die("Problemas con la conexion");
+
+
